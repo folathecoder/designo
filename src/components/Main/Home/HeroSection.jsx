de99657c0be95a7ffa6762f7f0879551e63516ff
folathecoder <officialakinloyefolarin@gmail.com>
2021-09-23 00:37:07 +0100

    Hero Section ==== Mobile

--- src/components/Main/Home/HeroSection.jsx
+++ src/components/Main/Home/HeroSection.jsx
@@ -9,17 +9,24 @@ import {
   HeroImage,
 } from "./HeroSectionStyles";
 import Button from "../../../components/global/Button/Button";
+import { handleScrollToTop } from "../../../helpers/Config";
 
 const HeroSection = ({ data }) => {
   console.log(data);
-  return (
+  return ( 
     <>
       <HeroContainer>
         <HeroWrap>
           <HeroContent>
             <HeroHeader>{data.header}</HeroHeader>
             <HeroSubHeader>{data.subHeader}</HeroSubHeader>
-            <Button primary>{data.button.title}</Button>
+            <Button
+              primary={true}
+              link={data.button.link}
+              click={handleScrollToTop}
+            >
+              {data.button.title}
+            </Button>
           </HeroContent>
           <HeroImageContainer>
             <HeroImage
