ba12f34adb9bbea1e412c42127294ede88650824
folathecoder <officialakinloyefolarin@gmail.com>
2021-09-27 13:02:43 +0100

    Fixed Deploy Bug

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Main, HiddenHeader } from "../global/GlobalStyles";
+import { Main } from "../global/GlobalStyles";
 import ContactCard from "../components/Main/Contact/ContactCard/ContactCard";
 import LocationList from "../components/Main/Location/LocationList/LocationList";
 import { locationListData } from "../data/pages/Location/LocationData";
